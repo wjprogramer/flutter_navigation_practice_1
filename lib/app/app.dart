b3e3dc8cc972dcb1a332915edd9445602cf7b50b
jay_wu <jay_wu>
2021-03-20 10:27:52 +0800

    simple routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation_practice_1/models/book.dart';
+import 'package:flutter_navigation_practice_1/screens/book_details_screen.dart';
 import 'package:flutter_navigation_practice_1/screens/book_list_screen.dart';
+import 'package:flutter_navigation_practice_1/screens/unknown_screen.dart';
 
 class BooksApp extends StatefulWidget {
   @override
@@ -23,18 +25,32 @@ class _BooksAppState extends State<BooksApp> {
 
   @override
   Widget build(BuildContext context) {
+
+    /// `Page`
+    ///
+    /// 當 `Page` 列表有變動，`Navigator` 會更新路徑的堆疊、符合 (match) 路徑
+    ///
+    /// - Navigator 會根據 `key` 不同，判斷成不同的 `Page`
+    final pages = [
+      MaterialPage(
+        key: ValueKey('BooksListPage'),
+        child: BooksListScreen(
+          books: books,
+          onTapped: _handleBookTapped,
+        ),
+      ),
+      if (show404)
+        MaterialPage(key: ValueKey('UnknownPage'), child: UnknownScreen())
+      else if (_selectedBook != null)
+        BookDetailsPage(
+          book: _selectedBook
+        )
+    ];
+
     return MaterialApp(
       title: 'Books App',
       home: Navigator(
-        pages: [
-          MaterialPage(
-            key: ValueKey('BooksListPage'),
-            child: BooksListScreen(
-              books: books,
-              onTapped: _handleBookTapped,
-            ),
-          )
-        ],
+        pages: pages,
         onPopPage: (route, result) => route.didPop(result),
       ),
     );
---
+++ lib/screens/book_details_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_practice_1/models/book.dart';
+
+class BookDetailsPage extends Page {
+  final Book book;
+
+  BookDetailsPage({
+    this.book,
+  }) : super(key: ValueKey(book));
+
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+      settings: this,
+      pageBuilder: (context, animation, animation2) {
+        final tween = Tween(begin: Offset(-1.0, 1.0), end: Offset.zero);
+        final curveTween = CurveTween(curve: Curves.easeInOut);
+        return SlideTransition(
+          position: animation.drive(curveTween).drive(tween),
+          child: BookDetailsScreen(
+            book: book,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BookDetailsScreen extends StatelessWidget {
+  final Book book;
+
+  BookDetailsScreen({
+    @required this.book,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (book != null) ...[
+              Text(book.title, style: Theme.of(context).textTheme.headline6),
+              Text(book.author, style: Theme.of(context).textTheme.subtitle1),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
