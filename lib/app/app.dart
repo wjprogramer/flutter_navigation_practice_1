a7b7ad819a91656fd9719e68d96369ffbb6bf0b3
jay_wu <jay_wu>
2021-03-20 10:03:41 +0800

    🎨

---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BooksApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Books App',
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            key: ValueKey('BooksListPage'),
+            child: Scaffold(),
+          )
+        ],
+        onPopPage: (route, result) => route.didPop(result),
+      ),
+    );
+  }
+}
